3ba4a97156ef600fb1ba73e4970a7103fb14dd85
Yaroslav Guz <aroslavguz@gmail.com>
2025-03-06 15:07:01 +0200

    Create input panel

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +12,26 @@ public class Main {
     }
 
     private static void initializeUI(JFrame frame) {
+        initializeInputPanel(frame);
+    }
+
+    private static void initializeInputPanel(JFrame frame) {
+        JPanel inputPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        JButton addStartPoint = new JButton("Add start point");
+        JButton repaintFractal = new JButton("Repaint fractal");
+        JLabel lineSizeLabel = new JLabel("Line size:");
+        JTextField lineSizeField = new JTextField(5);
+        JButton clearPanel = new JButton("Clear");
+        JButton drawDragon = new JButton("Draw dragon fractal");
+
+        inputPanel.add(addStartPoint);
+        inputPanel.add(repaintFractal);
+        inputPanel.add(lineSizeLabel);
+        inputPanel.add(lineSizeField);
+        inputPanel.add(clearPanel);
+        inputPanel.add(drawDragon);
+
+        frame.add(inputPanel);
     }
 }
