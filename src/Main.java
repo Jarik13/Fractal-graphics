1c85cb139b0299c5634359e5a72ee94cc886cf76
Yaroslav Guz <aroslavguz@gmail.com>
2025-03-06 14:58:51 +0200

    Init frame

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Fractal graphics!");
+        initializeUI(frame);
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
+
+    private static void initializeUI(JFrame frame) {
     }
 }
